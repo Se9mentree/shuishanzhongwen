a36ae6a665ecb0fecd90d6782a2189053b7f81d5
1BrianHe1 <2037245102@qq.com>
2025-11-06 14:50:04 +0800

    commit

--- src/app_bakcup1029/features/auth/__init__.py
+++
@@ -1,6 +0,0 @@
-# app/features/auth/__init__.py
-from .router import router
-from .service import AuthService
-from .schemas import LoginRequest, LoginResponse
-
-__all__ = ["router", "AuthService", "LoginRequest", "LoginResponse"]
--- src/app_bakcup1029/features/lesson_progress/__init__.py
+++
@@ -1,3 +0,0 @@
-from .router import router as lesson_progress_router
-
-__all__ = ["lesson_progress_router"]
